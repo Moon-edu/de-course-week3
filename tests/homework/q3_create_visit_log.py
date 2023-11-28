9d56b52dcede812bf45f7d4b011af50ad77ba1dc
Choi Hyeon Jun <che0223@naver.com>
2023-11-28 19:18:54 +0900

    호환성 수정 commit
    1. regexp_like -> x ~ pattern
    2. 'pattern' -> '^pattern$'

--- tests/homework/q3_create_visit_log.py
+++ tests/homework/q3_create_visit_log.py
@@ -5,7 +5,7 @@ def create_visit_log_table():
         with conn.cursor() as cur:
             cur.execute("""
                     create table visit_log(
-	                    visitor varchar(6) check(regexp_like(visitor, '[A-Z]\d{5}') or null),
+	                    visitor varchar(6) check(visitor ~ '^[A-Z]\d{5}$'),
 	                    enter timestamp not null,
 	                    out timestamp,
 	                    purpose varchar(50)
